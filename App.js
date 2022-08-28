34bf098436ed8a573557cfecb83c1ba53d1f8e8f
JoonaTeuho <joona.teuho@gmail.com>
2022-08-28 23:41:45 +0300

    added calculator functions

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 
 export default function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+
+  function Sum(){
+    setResult(parseInt(number1) + parseInt(number2));
+  }
+
+  function Subraction(){
+    setResult(number1 - number2);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result} </Text>
+      <TextInput
+      keyboardType='numeric'
+      style={styles.input}
+      onChangeText={number1 => setNumber1(number1)}  
+      value={number1}
+      />
+
+    <TextInput
+      keyboardType='numeric'
+      style={styles.input}
+      onChangeText={number2 => setNumber2(number2)}  
+      value={number2}
+      />
+      <Button onPress={Sum} title="+" />
+      <Button onPress={Subraction} title="-" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +46,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width:200 ,
+    borderColor: 'gray',
+    borderWidth:1
+  }
 });
